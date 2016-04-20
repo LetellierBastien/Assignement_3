ef54b145c4db546a3667e3f4edcad3ef900ae0d7
unknown <english@gcc-pc6030.student.gcc.ie>
2016-04-20 11:36:40 +0100

    Millestone1

---
+++ app/src/main/java/com/example/english/test/CustomView.java
@@ -0,0 +1,48 @@
+package com.example.english.test;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by english on 20/04/2016.
+ */
+public class CustomView extends View {
+
+    private int RecColor, labelCol;
+    private String circleText;
+    private Paint RecPaint;
+
+    public CustomView(Context context , AttributeSet attrs) {
+        super(context, attrs);
+
+
+        RecPaint = new Paint();
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs,
+                R.styleable.CustomView, 0, 0);
+        try {
+            RecColor = a.getInteger(R.styleable.CustomView_circleColor, 0);
+        } finally {
+            a.recycle();
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        int viewWidth = this.getMeasuredWidth();
+        int viewHeight = this.getMeasuredHeight();
+
+        RecPaint.setStyle(Paint.Style.FILL);
+        RecPaint.setAntiAlias(true);
+        RecPaint.setColor(RecColor);
+        canvas.drawRect(0, 0, viewHeight, viewWidth, RecPaint);
+
+
+
+    }
+
+
+}
