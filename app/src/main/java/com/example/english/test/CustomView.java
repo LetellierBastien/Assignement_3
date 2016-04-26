3e83095579a51f2a4c2e7b207a4bc0cd149f3547
steven <aqwpmn753@gmail.com>
2016-04-26 15:49:05 +0100

    MilleStone 5

--- app/src/main/java/com/example/english/test/CustomView.java
+++ app/src/main/java/com/example/english/test/CustomView.java
@@ -6,8 +6,13 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.location.Location;
+import android.os.Debug;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+
+import java.util.ArrayList;
 
 /**
  * Created by english on 20/04/2016.
@@ -17,6 +22,8 @@ public class CustomView extends View {
     private int RecColor, labelCol;
     private String circleText;
     private Paint RecPaint;
+    private ArrayList<Location> locaTab;
+    private int count=0;
 
     public CustomView(Context context , AttributeSet attrs) {
         super(context, attrs);
@@ -30,6 +37,12 @@ public class CustomView extends View {
         } finally {
             a.recycle();
         }
+    }
+
+    public void initLocaTab (ArrayList<Location> tab)
+    {
+        locaTab = tab;
+        invalidate();
     }
 
     @Override
@@ -47,6 +60,17 @@ public class CustomView extends View {
         for (int i=1;i<7;i++)
             canvas.drawLine(0,i*viewHeight/7,viewWidth,i*viewHeight/7,RecPaint);
 
+        if (locaTab != null)
+        {
+            RecPaint.setColor(Color.parseColor("#00FF00"));
+            float lastSpeed =0;
+
+            for(int i =0;i<locaTab.size();i++)
+            {
+                canvas.drawLine((i-1)*viewWidth/100,viewHeight-lastSpeed*viewHeight/60,i*viewWidth/100,viewHeight-locaTab.get(i).getSpeed()*3.6f*viewHeight/60,RecPaint);
+                lastSpeed = locaTab.get(i).getSpeed()*3.6f;
+            }
+        }
 
 
     }
--- app/src/main/java/com/example/english/test/MainActivity.java
+++ app/src/main/java/com/example/english/test/MainActivity.java
@@ -13,20 +13,34 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
     Button buttonStart;
+    TextView txt2, txt3,txt4;
     private boolean started=false;
     LocationManager locationManager;
     LocationListener locationListener;
-    Location [] loca = new Location[100];
-    int sample=0;
+    ArrayList<Location> loca = new ArrayList<Location>();
+    CustomView cstVw;
+    int call=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt2 = (TextView) findViewById(R.id.txt2);
+        txt3 = (TextView) findViewById(R.id.txt3);
+        txt4 = (TextView) findViewById(R.id.txt4);
+
+        txt2.setText(txt2.getText() + "N/A");
+        txt3.setText(txt3.getText() + "N/A");
+        txt4.setText(txt4.getText() + "N/A");
+
+        cstVw = (CustomView) findViewById(R.id.custView);
 
         buttonStart = (Button) findViewById(R.id.button1);
         buttonStart.setOnClickListener(new View.OnClickListener() {
@@ -48,10 +62,22 @@ public class MainActivity extends Activity {
         locationListener = new LocationListener() {
 
             public void onLocationChanged(Location location) {
-                Log.d("Location", "Changed");
-                loca[sample] = location;
-                sample++;
-                sample=sample%100;
+                loca.add(location);
+                if (loca.size()>100)
+                {
+                    loca.remove(0);
+                }
+                cstVw.initLocaTab(loca);
+
+                float x = 0;
+                for(int i=0;i<loca.size();i++)
+                    x = x+loca.get(i).getSpeed();
+
+                x=x/loca.size();
+                //txt2.setText("Current Speed: " + location.getSpeed()*3.6 + " Km/h");
+                //txt3.setText("Average Speed: " + x*3.6 + " Km/h");
+                //txt4.setText("Overall Time " + call + "s");
+                call++;
 
             }
 
@@ -67,7 +93,7 @@ public class MainActivity extends Activity {
         Log.i("Gps Status :", "Running");
         try
         {
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
 
         } catch (SecurityException e) {
 
